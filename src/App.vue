45b0e87b11781bd835135813c7a110dead7deab4
cmdfas <cmdfas@gmail.com>
2021-09-18 16:22:13 +0800

    安装Like-UI测试

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <li-button>like UI</li-button>
+  <li-hello-world msg="hello like UI"></li-hello-world>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import likeUI from 'like-ui'
+import 'like-ui/lib/like-ui.css'
+
+createApp(App).use(likeUI).mount('#app')
